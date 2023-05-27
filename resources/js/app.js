77ebad24c65db69fde10139517d6096222979c59
Kevin Labre <kevinlabre@gmail.com>
2023-05-27 08:54:31 +0200

    Installation vuejs with vite plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+import {createApp} from "vue/dist/vue.esm-bundler";
+
+import Test from "./components/test.vue"
+
+const app = createApp({
+    components: {
+        Test
+    }
+})
+
+app.mount("#app")
