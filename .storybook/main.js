ef929d34f7d551b322bd506e21e5b0c990adb93b
Kevin Labre <kevinlabre@gmail.com>
2023-05-27 22:24:44 +0200

    Update package json, Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,24 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: [
-      "../resources/js/components/*.mdx",
-      "../resources/js/components/*.stories.@(js|jsx|ts|tsx)",
+    stories: [
+        "../resources/js/components/*.mdx",
+        "../resources/js/components/*.stories.@(js|jsx|ts|tsx)",
 
-      "../resources/js/components/**/*.mdx",
-      "../resources/js/components/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/vue3-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+        "../resources/js/components/**/*.mdx",
+        "../resources/js/components/**/*.stories.@(js|jsx|ts|tsx)",
+    ],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/addon-styling",
+    ],
+    framework: {
+        name: "@storybook/vue3-vite",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
 };
 export default config;
