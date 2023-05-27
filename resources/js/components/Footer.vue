cc877fb708debe771bd0f2301b5e7169f68be1f7
Kevin Labre <kevinlabre@gmail.com>
2023-05-27 22:23:32 +0200

    Add Footer Component

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,260 @@
+<template>
+	<footer class="mt-6 text-white background-radar">
+		<div class="container py-5">
+			<div class="row">
+				<div class="mb-4 col-12 mobile-stores">
+					<p class="mb-3 text-center text-weight-500">
+						Disponible sur
+					</p>
+
+					<div class="d-flex justify-content-center">
+						<div>
+							<img
+								src="https://www.navily.com/images/icons/social/download/download-navily-app-store-fr.png"
+								loading="lazy"
+								alt="Logo Apple store"
+								onerror="this.src='https://www.navily.com/images/icons/social/download/download-navily-app-store-en.png'"
+								class="mx-3 mb-2 pointer"
+							>
+						</div>
+
+						<div>
+							<img
+								src="https://www.navily.com/images/icons/social/download/download-navily-play-store-fr.png"
+								loading="lazy"
+								alt="Logo Play store"
+								onerror="this.src='https://www.navily.com/images/icons/social/download/download-navily-play-store-en.png'"
+								class="mx-3 pointer"
+							>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 infos">
+					<img
+						src="https://www.navily.com/images/logos/logo-white.svg"
+						loading="lazy"
+						alt="Logo Navily"
+						class="mb-2 logo"
+					>
+
+					<div class="mb-3 social">
+						<ul class="d-flex align-items-center text-md-plus text-main-light-color ">
+							<li class="mr-3">
+								<a href="https://www.facebook.com/navilyApp" target="_blank" title="Facebook">
+									<img
+										src="https://www.navily.com/images/icons/social/facebook.png"
+										loading="lazy"
+										alt="Logo Facebook"
+									>
+								</a>
+							</li>
+
+							<li class="mr-3">
+								<a href="https://www.instagram.com/navilyapp" target="_blank" title="Instagram">
+									<img
+										src="https://www.navily.com/images/icons/social/instagram.png"
+										loading="lazy"
+										alt="Logo Instagram"
+									>
+								</a>
+							</li>
+
+							<li class="mr-3">
+								<a href="https://twitter.com/navilyapp" target="_blank" title="Twitter">
+									<img
+										src="https://www.navily.com/images/icons/social/twitter.png"
+										loading="lazy"
+										alt="Logo Twitter"
+									>
+								</a>
+							</li>
+
+							<li class="mr-3">
+								<a
+									href="https://www.youtube.com/channel/UCKtZfhuVFBLpLaTAfEM-cQg"
+									target="_blank"
+									title="Youtube"
+								>
+									<img
+										src="https://www.navily.com/images/icons/social/youtube.png"
+										loading="lazy"
+										alt="Logo Youtube"
+									>
+								</a>
+							</li>
+						</ul>
+					</div>
+
+					<p class="text-sm-plus">
+						Navily est le guide côtier communautaire leader en Europe avec +200 000
+						contributions. Réservez également en 3 clics dans nos nombreux ports partenaires répartis sur
+						les côtes de plus de 10 pays.
+					</p>
+
+					<div class="mb-3 mobile-social">
+						<ul class="d-flex align-items-center text-md-plus text-main-light-color">
+							<li class="mr-3">
+								<a href="https://www.facebook.com/navilyApp" target="_blank" title="Facebook">
+									<img
+										src="https://www.navily.com/images/icons/social/facebook.png"
+										loading="lazy"
+										alt="Logo Facebook"
+									>
+								</a>
+							</li>
+
+							<li class="mr-3">
+								<a href="https://www.instagram.com/navilyapp" target="_blank" title="Instagram">
+									<img
+										src="https://www.navily.com/images/icons/social/instagram.png"
+										loading="lazy"
+										alt="Logo Instagram"
+									>
+								</a>
+							</li>
+
+							<li class="mr-3">
+								<a href="https://twitter.com/navilyapp" target="_blank" title="Twitter">
+									<img
+										src="https://www.navily.com/images/icons/social/twitter.png"
+										loading="lazy"
+										alt="Logo Twitter"
+									>
+								</a>
+							</li>
+
+							<li class="mr-3">
+								<a
+									href="https://www.youtube.com/channel/UCKtZfhuVFBLpLaTAfEM-cQg"
+									target="_blank"
+									title="Youtube"
+								>
+									<img
+										src="https://www.navily.com/images/icons/social/youtube.png"
+										loading="lazy"
+										alt="Logo Youtube"
+									>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-lg-6 offset-lg-1 d-flex menus">
+					<nav>
+						<p class="mb-3 text-weight-700">
+							Navigation
+						</p>
+
+						<ul class="text-main-light-color">
+							<li class="mb-2">
+								<a href="https://www.navily.com/fr" title="Page d'accueil">
+									Page d'accueil
+								</a>
+							</li>
+
+							<li class="mb-2">
+								<a href="https://www.navily.com/fr/carte" title="Explorer la carte">
+									Explorer la carte
+								</a>
+							</li>
+
+							<li class="mb-2">
+								<a href="http://blog.navily.com/" title="Blog" target="_blank">
+									Blog
+								</a>
+							</li>
+						</ul>
+					</nav>
+
+					<nav>
+						<p class="mb-3 text-weight-700">
+							Compte
+						</p>
+
+						<ul class="text-main-light-color">
+							<li class="mb-2">
+								<a href="https://www.navily.com/fr/navily-premium" title="Navily Premium">
+									Navily Premium
+								</a>
+							</li>
+
+							<li class="mb-2">
+								<a href="https://www.navily.com/fr/port/discover" title="Navily Premium">
+									Espace port
+								</a>
+							</li>
+						</ul>
+					</nav>
+
+					<div class="stores">
+						<p class="mb-3 text-weight-700">
+							Découvrir l'application
+						</p>
+
+						<div class="d-flex flex-column">
+							<a href="https://apps.apple.com/fr/app/navily/id881103205" target="_blank">
+								<img
+									src="https://www.navily.com/images/icons/social/download/download-navily-app-store-fr.png"
+									loading="lazy"
+									alt="Logo Apple store"
+									onerror="this.src='https://www.navily.com/images/icons/social/download/download-navily-app-store-en.png'"
+									class="mb-2 pointer"
+								>
+							</a>
+
+							<a
+								href="https://play.google.com/store/apps/details?id=com.ckcnet.android.navily&amp;hl=fr"
+								target="_blank"
+							>
+								<img
+									src="https://www.navily.com/images/icons/social/download/download-navily-play-store-fr.png"
+									loading="lazy"
+									alt="Logo Play store"
+									onerror="this.src='https://www.navily.com/images/icons/social/download/download-navily-play-store-en.png'"
+									class="pointer"
+								>
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-2 d-flex justify-content-end preview">
+					<picture>
+						<source srcset="https://www.navily.com/images/home/discover/preview.webp" type="image/webp">
+						<source srcset="https://www.navily.com/images/home/discover/preview.png" type="image/jpg">
+
+						<img
+							src="https://www.navily.com/images/home/discover/preview.png"
+							loading="lazy"
+							alt="Smartphone"
+						>
+					</picture>
+				</div>
+			</div>
+		</div>
+
+		<p class="pb-5 text-center text-sm-plus text-grey-medium">
+			© The Navigation Family 2023 —
+			<a href="https://www.navily.com/fr/mentions-legales">
+				Mentions légales
+			</a>
+			—
+			<a href="https://www.navily.com/fr/privacy">
+				Politique de confidentialité
+			</a>
+			—
+			<a href="https://www.navily.com/fr/cgu">
+				CGU
+			</a>
+		</p>
+	</footer>
+</template>
+
+<script>
+
+export default {
+	props: {},
+};
+</script>
