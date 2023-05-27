2a59a5f9827e6ae5955f86fbb2c191042839dfd9
Kevin Labre <kevinlabre@gmail.com>
2023-05-27 22:23:42 +0200

    Register components

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,14 @@ import './bootstrap';
 
 import {createApp} from "vue/dist/vue.esm-bundler";
 
-import Test from "./components/test.vue"
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
 
-const app = createApp({
-    components: {
-        Test
-    }
-})
+import 'bootstrap'
+
+const app = createApp({})
+
+app.component('header-component', Header);
+app.component('footer-component', Footer);
 
 app.mount("#app")
