c4523d4eb5a7181fba94bf67d5f2d4dab90b5e60
Kevin Labre <kevinlabre@gmail.com>
2023-05-29 22:21:17 +0200

    Front informations account view

--- resources/views/account/informations.blade.php
+++ resources/views/account/informations.blade.php
@@ -8,7 +8,43 @@
             </div>
 
             <div class="col-9">
+                <h1 class="text-xl-plus text-weight-700 text-center text-main-deep-color pb-3">
+                    Informations
+                </h1>
+                
+                <div class="d-flex justify-content-center">
+                    <img src="/images/user/kevin-linkedin.jpeg" alt="Kevin Labre" class="rounded-full">
+                </div>
 
+                <div class="text-center pb-5">
+                    <p class="text-main-deep-color pt-4 text-weight-700">
+                        Kevin Labre
+                    </p>
+
+                    <p class="text-grey-medium">
+                        Profil completé à 17%
+                    </p>
+
+                    <p class="text-main-deep-color pt-4 text-weight-700">
+                        Debutant
+                    </p>
+
+                    <p class="text-main-light-color ">
+                        10 points
+                    </p>
+                </div>
+
+                <div class="deep-blue-card md-rounded p-5" style="cursor: pointer">
+                    <div class="row">
+                        <div class="col-6">
+                            <img src="/images/logo/logo-premium.svg" alt="Navily premium" class="w-100">
+                        </div>
+
+                        <div class="col-6 d-flex justify-content-end align-items-center">
+                            <i class="fa-solid fa-arrow-right text-white" style="font-size: 50px"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
