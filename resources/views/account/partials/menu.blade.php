be38eaa6320b751b8daf5b7f98772e1a4fe2185b
Kevin Labre <kevinlabre@gmail.com>
2023-05-29 21:55:15 +0200

    Fix position icon menu

--- resources/views/account/partials/menu.blade.php
+++ resources/views/account/partials/menu.blade.php
@@ -1,90 +1,90 @@
 <ul class="list-group list-account ">
     <a class="list-group-item active" href="{{ route('account.informations') }}">
-        <i class="fa-solid fa-info pr-4"></i>
+        <i class="fa-solid fa-info" style="width: 40px"></i>
 
         Informations
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-person pr-4"></i>
+        <i class="fa-solid fa-person" style="width: 40px"></i>
 
         Skipper
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-ship pr-4"></i>
+        <i class="fa-solid fa-ship" style="width: 40px"></i>
 
         Bateau
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-file pr-4"></i>
+        <i class="fa-solid fa-file" style="width: 40px"></i>
 
         Documents
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-credit-card pr-4"></i>
+        <i class="fa-solid fa-credit-card" style="width: 40px"></i>
 
         Paiement
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-cogs pr-4"></i>
+        <i class="fa-solid fa-cogs" style="width: 40px"></i>
 
         Parametres
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-lock pr-4"></i>
+        <i class="fa-solid fa-lock" style="width: 40px"></i>
 
         Changer mot de passe
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-share-nodes pr-4"></i>
+        <i class="fa-solid fa-share-nodes" style="width: 40px"></i>
 
         Inviter un ami
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-heart pr-4"></i>
+        <i class="fa-solid fa-heart" style="width: 40px"></i>
 
         Favoris
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-question pr-4"></i>
+        <i class="fa-solid fa-question" style="width: 40px"></i>
 
         Aide
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-newspaper pr-4"></i>
+        <i class="fa-solid fa-newspaper" style="width: 40px"></i>
 
         Tutoriels
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-map-location-dot pr-4"></i>
+        <i class="fa-solid fa-map-location-dot" style="width: 40px"></i>
 
         Legende de la carte
     </a>
 
     <a href="#" class="list-group-item">
-        <i class="fa-solid fa-comments pr-4"></i>
+        <i class="fa-solid fa-comments" style="width: 40px"></i>
 
         Chattez avec nous
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-circle-info pr-4"></i>
+        <i class="fa-solid fa-circle-info" style="width: 40px"></i>
 
         A propos de Navily
     </a>
 
     <a href="" class="list-group-item">
-        <i class="fa-solid fa-user-slash pr-4"></i>
+        <i class="fa-solid fa-user-slash" style="width: 40px"></i>
 
         Deconnexion
     </a>
