ee5facb8c43c6d943b2ddf4e9c17ff59c495ced3
Kevin Labre <kevinlabre@gmail.com>
2023-05-31 14:22:59 +0200

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,9 @@ class DatabaseSeeder extends Seeder
              'password' => 'password',
 			 'first_name' => 'Kevin',
 			 'last_name' => 'Labre',
+			 'birth_date' => '1997-10-09',
+			 'description' => 'Hello world',
+			 'phone' => '+33699999999',
 			 'image' => 'data:image/png;base64,' . base64_encode(file_get_contents(__DIR__ . '/images/users/kevin-linkedin.jpeg'))
          ]);
     }
