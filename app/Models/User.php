fafb5d1e72aa9e93dad58bad916c2d23ccc3424c
Kevin Labre <kevinlabre@gmail.com>
2023-05-31 08:15:02 +0200

    update users table / Model / Factory

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,40 +6,92 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+	use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+	const LEVELS = [
+		self::LEVEL_0,
+		self::LEVEL_1,
+		self::LEVEL_2
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
+	const LEVEL_0 = 'Debutant';
+	const LEVEL_1 = 'Confirmé';
+	const LEVEL_2 = 'Expert';
+
+	protected $fillable = [
+		'email',
+		'password',
+
+		'first_name',
+		'last_name',
+		'phone',
+		'description',
+		'birth_date',
+
+		'level',
+		'points',
+
+		'image',
+	];
+
+	protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+	public function getFullNameAttribute(): string
+	{
+		return "$this->first_name $this->last_name";
+	}
+
+	public function	getPointsDisplayAttribute(): string
+	{
+		$text_points = $this->points < 2
+			? Str::plural('point', 1)
+			: Str::plural('point', $this->points);
+
+		return $this->points . ' ' . $text_points;
+	}
+
+	public function getProgressionAttribute(): int
+	{
+		$progression = 0;
+
+		$attributes = [
+			$this->phone,
+			$this->last_name,
+			$this->first_name,
+			$this->description,
+			$this->image,
+			$this->birth_date
+		];
+
+		$progression_by_attribute_filled = 100 / count($attributes);
+
+		foreach($attributes as $attribute)
+		{
+			if($attribute != null)
+			{
+				$progression += $progression_by_attribute_filled;
+			}
+		}
+
+		return $progression;
+	}
+
+	public function getDefaultImageAttribute(): string
+	{
+		return 'data:image/png;base64,' . base64_encode(file_get_contents(public_path('/images/logo/logo-primary.svg')));
+	}
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 /**
@@ -10,25 +12,64 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+		$level = fake()
+			->randomElement(User::LEVELS);
+
+		if($level == User::LEVEL_0)
+		{
+			$point_min = 0;
+			$point_max = 100;
+		}
+		elseif($level == User::LEVEL_1)
+		{
+			$point_min = 101;
+			$point_max = 200;
+		}
+		else
+		{
+			$point_min = 201;
+			$point_max = 300;
+		}
+
+		$points = fake()
+			->numberBetween($point_min, $point_max);
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => fake()
+				->unique()
+				->safeEmail(),
+
             'email_verified_at' => now(),
+
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+
+			'last_name' => fake()
+				->lastName(),
+
+			'first_name' => fake()
+				->firstName(),
+
+			'phone' => fake()
+				->phoneNumber(),
+
+			'description' => fake()
+				->text(),
+
+			'birth_date' => fake()
+				->dateTimeBetween('-30 years', "-20 years"),
+
+			'level' => $level,
+
+			'points' => $points,
+
+			'image' => null,
+
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
