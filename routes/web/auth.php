0da8ecd459bd24e1e96d93856441c862cef3ef37
Kevin Labre <kevinlabre@gmail.com>
2023-05-31 14:22:48 +0200

    explode controllers

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')
@@ -8,9 +9,5 @@ Route::middleware('guest')
 	->name('login');
 
 Route::middleware('auth')
-	->get('/logout', function()
-	{
-		auth()->logout();
-		return redirect(route('login'));
-	})
+	->get('/logout', [LogoutController::class, 'logout'])
 	->name('logout');
