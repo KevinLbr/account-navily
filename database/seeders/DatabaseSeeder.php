b6c260897f7e526f3c6edd63e33d933b7a59b5e3
Kevin Labre <kevinlabre@gmail.com>
2023-05-31 08:15:12 +0200

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,9 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
          User::factory()->create([
-             'name' => 'Test User',
              'email' => 'kevin@navily.com',
              'password' => 'password',
+			 'first_name' => 'Kevin',
+			 'last_name' => 'Labre',
+			 'image' => 'data:image/png;base64,' . base64_encode(file_get_contents(__DIR__ . '/images/users/kevin-linkedin.jpeg'))
          ]);
     }
 }
